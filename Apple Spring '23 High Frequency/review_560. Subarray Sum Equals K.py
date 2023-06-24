db46df2eea7e405e6f1eff20d7f52e57f265e0b7
Suji <sujioh5@gmail.com>
2023-06-23 18:34:02 -0700

    subarray sum - review will be needed

---
+++ Apple Spring '23 High Frequency/review_560. Subarray Sum Equals K.py
@@ -0,0 +1,31 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefixSum = 0
+        d = {0: 1}
+        ans = 0
+
+        for i in range(len(nums)):
+            prefixSum = prefixSum + nums[i]
+            if prefixSum - k in d:
+                ans = ans + d[prefixSum-k]
+
+            if prefixSum in d:
+                d[prefixSum] = d[prefixSum] + 1
+            else:
+                d[prefixSum] = 1
+
+        return ans
+
+
+s = Solution()
+print(s.subarraySum([1, 2, 3], 3))
+
+
+'''
+Summary:
+Review needed. 
+
+Complexity:
+ - Time: O(n): n is the number of chars
+ - Space: O(n): n is length of chars 
+'''
