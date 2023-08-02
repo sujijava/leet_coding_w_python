c5db3586a42ea20f729a0223e36cfefc8de494a4
Suji <sujioh5@gmail.com>
2023-08-01 18:15:51 -0700

    i don't get it that much

---
+++ Top Interview 75/283. Move Zeroes.py
@@ -0,0 +1,11 @@
+from ast import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l = l + 1
+        return nums
