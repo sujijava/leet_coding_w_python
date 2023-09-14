0db3b298f9ba9255144ba7d551d5b8735189a881
Suji <sujioh5@gmail.com>
2023-09-13 19:34:22 -0700

    sept12

--- Top Interview 75/334. Increasing Triplet Subsequence.py
+++ Top Interview 75/334. Increasing Triplet Subsequence.py
@@ -1,12 +1,12 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        small = nums[0]
-        mid = nums[0]
+        small = float('inf')
+        mid = float('inf')
 
         for num in nums:
             if num <= small:
                 small = num
-            elif num >= mid:
+            elif num <= mid:
                 mid = num
             else:
                 return True
