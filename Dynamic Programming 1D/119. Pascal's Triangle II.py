572932bb5a55388ba940cc1cc308c84bee7acfc5
Suji <sujioh5@gmail.com>
2023-11-09 18:42:15 -0800

    pascal 2

--- Dynamic Programming 1D/119. Pascal's Triangle II.py
+++ Dynamic Programming 1D/119. Pascal's Triangle II.py
@@ -1,20 +1,10 @@
-import pdb
-from typing import List
-
-class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex == 0:
-            return [1]
-        if rowIndex == 1:
-            return [1,1]
-
-        row = [1,1]
-        for i in range(1,rowIndex):
-            prevRow = row
-            pdb.set_trace()  # This line sets a breakpoint
-            row.insert(i, prevRow[i-1]+prevRow[i])
-            print(prevRow)
-        return row 
-
-s = Solution()
-s.getRow(3)
+class Solution(object):
+    def getRow(self, r):
+        ans = [1]*(r+1)
+        up = r
+        down = 1
+        for i in range(1, r):
+            ans[i] = ans[i-1]*up/down;
+            up = up - 1
+            down = down + 1
+        return ans
