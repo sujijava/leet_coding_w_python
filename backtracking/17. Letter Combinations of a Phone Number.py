3684ff62bbb14a2f821f5db18ed0b41e850bfb7b
Suji <sujioh5@gmail.com>
2023-11-15 16:14:11 -0800

    yday

--- backtracking/17. Letter Combinations of a Phone Number.py
+++ backtracking/17. Letter Combinations of a Phone Number.py
@@ -5,14 +5,17 @@ class Solution:
         
         phone = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
         res = []
-        
-        def backtrack(combination, next_digits):
+        temp = []
+
+        def backtrack(next_digits):
             if not next_digits:
-                res.append(combination)
+                res.append("".join(temp))
                 return
             
             for letter in phone[next_digits[0]]:
-                backtrack(combination + letter, next_digits[1:])
+                temp.append(letter)
+                backtrack(next_digits[1:])
+                temp.pop()
         
-        backtrack("", digits)
+        backtrack(digits)
         return res
