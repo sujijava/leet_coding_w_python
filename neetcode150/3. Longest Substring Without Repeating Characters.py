7926b9e1a8cdec8fd9f508c76d0a265a5caf6a97
Suji Oh <suji-inkblot@Sujis-MacBook-Pro.local>
2024-05-27 18:24:38 -0700

    MAY 27 - readbility of question no.3

--- neetcode150/3. Longest Substring Without Repeating Characters.py
+++ neetcode150/3. Longest Substring Without Repeating Characters.py
@@ -4,40 +4,19 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_arr = []
-        l = 0
-        r = 0
-        res = 0
-
-        while r < len(s):
-            cur_char = s[r]
-
-            if cur_char in char_arr:
-                char_arr = []
-                l = r + 1
-
-            char_arr.append(cur_char)
-
-            res = max(r - l + 1, res)
-
-            r = r+1
-        return res
-
-
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         charSet = set()
-#         l = 0
-#         res = 0
-
-#         for r in range(len(s)):
-#             while s[r] in charSet:
-#                 charSet.remove(s[l])
-#                 l += 1
-#             charSet.add(s[r])
-#             res = max(res, r - l + 1)
-#         return res
-
+        l = 0 
+        counter = 1
+        
+        if len(s) == 0:
+          return 0
+        
+        for r in range(1,len(s)): # r = 1 # r = 2
+          while s[r] in s[l:r]: 
+            # should include l and r characters    
+              l += 1 # r = 1
+          temp_counter = r - l + 1  # 1-0+1 = 2
+          counter = max(temp_counter, counter) # 2 
+        return counter
 
 s = Solution()
 s.lengthOfLongestSubstring("aab")
