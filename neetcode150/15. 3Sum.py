6db2470777a3834209d1bc3726d1b5f5d3951c5e
Suji <sujioh5@gmail.com>
2024-06-16 21:20:58 -0700

    3sum add comments review

--- neetcode150/15. 3Sum.py
+++ neetcode150/15. 3Sum.py
@@ -1,26 +1,32 @@
+from typing import List
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        nums.sort()  # Sort the array first
         res = []
 
         for l in range(len(nums)):
-            if nums[l] > 0:
-                break
+            # Skip duplicate elements for l
             if l > 0 and nums[l] == nums[l - 1]:
                 continue
-
             m = l + 1
             r = len(nums) - 1
+
             while m < r:
                 curr_sum = nums[l] + nums[m] + nums[r]
-                if curr_sum > 0:
-                    r -= 1
-                elif curr_sum < 0:
-                    m += 1
-                else:
+                if curr_sum == 0:
                     res.append([nums[l], nums[m], nums[r]])
                     m += 1
                     r -= 1
-                    while m < r and nums[m] == nums[m-1]:
+                    # Skip duplicate elements for m
+                    while m < r and nums[m] == nums[m - 1]:
                         m += 1
-        return res
+                    # # Skip duplicate elements for r
+                    # code actually works with or without it. WHY?
+                    # while m < r and nums[r] == nums[r + 1]:
+                    #     r -= 1
+                elif curr_sum > 0:
+                    r -= 1
+                else:
+                    m += 1
+        return res 
